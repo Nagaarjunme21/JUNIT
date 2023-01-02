1814314ad710c1d425220e56a0189162aa76b463
Nagaarjunme21 <nagaarjunme@gmail.com>
2023-01-02 20:28:10 +0530

    fourth commit

---
+++ SeleniumDemo/src/test/java/com/demo/SeleniumDemo/TestRunner.java
@@ -0,0 +1,14 @@
+package com.demo.SeleniumDemo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Orange.class,Orange1.class}) 
+public class TestRunner {
+
+	
+
+	
+
+}
